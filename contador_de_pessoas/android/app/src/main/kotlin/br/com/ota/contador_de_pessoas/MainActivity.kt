9fee5b1b7e7b73dfc29aab7b54f1141e34b9a4dd
ota-renato <30956931+ota-renato@users.noreply.github.com>
2020-07-09 18:35:38 -0300

    Contador de pessoas

---
+++ contador_de_pessoas/android/app/src/main/kotlin/br/com/ota/contador_de_pessoas/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.ota.contador_de_pessoas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ contador_de_pessoas/lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      title: "Contador de Pessoas",
+      home: Home()
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _people = 0;
+  String _textoMensagem = "Restaurante vazio";
+  void _changePeople(int delta) {
+    setState(() {
+      _people += delta;
+      if(_people <= 0) {
+        _people = 0;
+        _textoMensagem = "Restaurante vazio";
+      } else if(_people > 0 && _people < 10) {
+        _textoMensagem = "Pode entrar!";
+      } else {
+        _people = 10;
+        _textoMensagem = "Restaurante lotado!";
+      }
+    });
+  }
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "images/restaurant.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Pessoas: $_people",
+              style:
+              TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "+",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
+                    ),
+                    onPressed: () {
+                      _changePeople(1);
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "-",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
+                    ),
+                    onPressed: () {
+                      _changePeople(-1);
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              _textoMensagem,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30.0),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
+
